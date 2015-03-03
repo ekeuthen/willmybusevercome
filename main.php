f42ebcd448ca62a683eb9dea4c992a889063c226
Emily Keuthen <ekeuthen@gmail.com>
2015-03-02 21:20:58 -0500

    adding fuctions for getting bus time prediction

--- main.php
+++ main.php
@@ -1,2 +1,86 @@
 <?php
-    echo 'Hello Buggy!';
+	
+	function getPredictionForStop($apiKey, $queryParams) {
+		return file_get_contents("http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=$apiKey&format=json&$queryParams");
+	}
+
+	function parsePredictions($json, $directionId, $busNumber) {
+		$predictions = array();
+		// get all modes
+		$modes = $json["mode"];
+
+		// select bus mode
+		foreach ($modes as $mode) {
+			if ($mode["mode_name"] == "Bus") {
+				$routes = $mode["route"];
+				// for each route
+				foreach ($routes as $route) {
+					if ($route["route_id"] == $busNumber) {
+						// get all directions
+						$directions = $route["direction"];
+						// for each direction
+						foreach ($directions as $direction) {
+							// check direction = 0 (outbound)
+							if ($direction["direction_id"] == $directionId) {
+								// get trips
+								$trips = $direction["trip"];
+								// display all trips
+								foreach ($trips as $trip) {
+									array_push($predictions, $trip["pre_away"]);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return $predictions;
+	}
+
+	function printPredictions($busNumber, $predictionsArray) {
+		echo "<h4>$busNumber</h4>";
+		echo "<ul>";
+		foreach ($predictionsArray as $prediction) {
+			echo "<li>$prediction</li>";
+		}
+		echo "</ul>";
+	}
+?>
+
+<html>
+	<head>
+		<title>Will Bus Ever Come</title>
+	</head>
+
+	<body>
+
+	<?php
+		$apiKey = 'wX9NwuHnZU2ToO7GmGR9uw';
+		$bus87 = '87';
+		$bus88 = '88';
+		$outboundId = "0";
+
+		echo '<h2>To work</h2>';
+		// bus 87
+
+		// bus 88
+
+		echo '<h2>Home!</h2>';
+		$lechmereStopId = '14150';
+		$predictions = getPredictionForStop($apiKey, "stop=$lechmereStopId");
+		$jsonPredictions = json_decode($predictions, true);
+
+		// bus 87
+		// parse predictions
+		$predictions = parsePredictions($jsonPredictions, $outboundId, $bus87);
+		// print predictions
+		printPredictions($bus87, $predictions);
+
+		// bus 88
+		// parse predictions
+		$predictions = parsePredictions($jsonPredictions, $outboundId, $bus88);
+		// print predictions
+		printPredictions($bus88, $predictions);
+	?>
+ 	</body>
+ </html>
